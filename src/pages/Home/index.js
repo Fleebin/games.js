50aab39a42411f9895538a4a484108869b165ea5
Felipe <felipemosilvaa@outlook.com>
2022-02-03 12:36:51 -0300

    Feature: Colocando os jogos dentro do card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 import { api } from '../../services/api'
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
 export const Home = () => {
     const [games, setGames] = useState([])
 
@@ -18,13 +26,32 @@ export const Home = () => {
     }
 
     return (
-        <>
-            {games.map(({ name, background_image }) => (
-                <li key={name}>
-                    {name}
-                    <img src={background_image} />
-                </li>
-            ))}
-        </>
+        <Container maxWidth="md">
+            <Box
+                sx={{
+                    display: 'grid',
+                    columnGap: 4,
+                    rowGap: 3,
+                    gridTemplateColumns: 'repeat(3, 1fr)',
+                }}
+            >
+                {games.map(({ name, background_image, id }) => (
+                    <Card sx={{ maxWidth: 345 }} key={id}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={background_image}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {name}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
+            </Box>
+        </Container>
     );
 }
